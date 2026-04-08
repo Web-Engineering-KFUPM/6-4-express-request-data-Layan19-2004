a5892bf1768bb90cd99b395ccf2565dda206c085
Layan-2004 <layanalasmari25@gmail.com>
2026-04-08 13:01:31 +0300

    modifications

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -109,7 +109,7 @@ import express from "express";
 
 // create express app instance to create web server
 const app = express();
-app.listen(3000, ()=> console.log("API running at http://localhost:3000"));
+
 
 
 // Query params: /echo?name=Ali&age=22
@@ -144,5 +144,5 @@ app.get("/users/:userId", (req,res)=>{ res.json({
    ok:true, userId: req.userIdNum }); });
 
 // Start the server by listening
-
+app.listen(3000, ()=> console.log("API running at http://localhost:3000"));
 
